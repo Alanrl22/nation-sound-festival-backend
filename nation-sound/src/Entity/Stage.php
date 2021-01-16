5538a68c21252c58ca37e756735f3ba47411d627
Alanrl22 <61279313+Alanrl22@users.noreply.github.com>
2021-01-16 14:43:46 +0100

    creation of the Stage entity and attributes

---
+++ nation-sound/src/Entity/Stage.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StageRepository::class)
+ */
+class Stage
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $coordinate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCoordinate(): ?string
+    {
+        return $this->coordinate;
+    }
+
+    public function setCoordinate(string $coordinate): self
+    {
+        $this->coordinate = $coordinate;
+
+        return $this;
+    }
+}
