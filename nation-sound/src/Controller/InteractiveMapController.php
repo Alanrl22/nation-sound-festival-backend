9686e567177988cad619dbd6f26e95a382f8e283
Alanrl22 <61279313+Alanrl22@users.noreply.github.com>
2021-01-16 17:05:35 +0100

    creation of Contact, GeneralInformationsActuality, InteractiveMap, MeetArtists, Partners Controllers

---
+++ nation-sound/src/Controller/InteractiveMapController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InteractiveMapController extends AbstractController
+{
+    /**
+     * @Route("/interactive/map", name="interactive_map")
+     */
+    public function index(): Response
+    {
+        return $this->render('interactive_map/index.html.twig', [
+            'controller_name' => 'InteractiveMapController',
+        ]);
+    }
+}
---
+++ nation-sound/src/Controller/MeetArtistsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MeetArtistsController extends AbstractController
+{
+    /**
+     * @Route("/meet/artists", name="meet_artists")
+     */
+    public function index(): Response
+    {
+        return $this->render('meet_artists/index.html.twig', [
+            'controller_name' => 'MeetArtistsController',
+        ]);
+    }
+}
