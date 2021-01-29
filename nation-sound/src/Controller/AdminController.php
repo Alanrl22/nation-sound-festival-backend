675151afff345665bd358ddbf44e717e798efd6b
Gohon95 <52068272+Gohon95@users.noreply.github.com>
2021-01-29 10:34:18 +0100

    Update AdminController.php
    admin_

--- nation-sound/src/Controller/AdminController.php
+++ nation-sound/src/Controller/AdminController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/admin", name="admin")
+ * @Route("/admin", name="admin_")
  */
 class AdminController extends AbstractController
 {
